b9cc28bd0e78cc8032484929f2948b4d5fe7871d
dlrjsals97 <kmlee97s@gmail.com>
2022-12-11 03:22:26 +0900

    feat: note_db를 repository에서 참조하도록

--- lib/data/repository/note_repository_impl.dart
+++ lib/data/repository/note_repository_impl.dart
@@ -1,15 +1,32 @@
+import 'package:flutter_noteapp/data/data_source/note_db.dart';
 import 'package:flutter_noteapp/domain/model/note.dart';
 import 'package:flutter_noteapp/domain/repository/note_repository.dart';
 
 class NoteRepositoryImpl implements NoteRepository {
+  final NoteDB db;
+  NoteRepositoryImpl(this.db);
   @override
-  Future<List<Note>> getNotes() {}
+  Future<List<Note>> getNotes() {
+    throw UnimplementedError();
+  }
+
   @override
-  Future<Note> getNoteById(int id) {}
+  Future<Note> getNoteById(int id) {
+    throw UnimplementedError();
+  }
+
   @override
-  Future<void> insertNote(Note note) {}
+  Future<void> insertNote(Note note) {
+    throw UnimplementedError();
+  }
+
   @override
-  Future<void> updateNote(Note note) {}
+  Future<void> updateNote(Note note) {
+    throw UnimplementedError();
+  }
+
   @override
-  Future<void> deleteNote(Note note) {}
+  Future<void> deleteNote(Note note) {
+    throw UnimplementedError();
+  }
 }
