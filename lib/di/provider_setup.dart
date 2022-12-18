607df92709fa1eb3a8957a3aa72cd4e8b334a56e
Leegeonmin <kmlee97s@gmail.com>
2022-12-18 20:56:26 +0900

    feat: provider 의존성 설정 파일

--- lib/di/provider_setup.dart
+++ lib/di/provider_setup.dart
@@ -1,13 +1,26 @@
+import 'package:flutter_noteapp/data/data_source/note_db_helper.dart';
+import 'package:flutter_noteapp/data/repository/note_repository_impl.dart';
+import 'package:flutter_noteapp/domain/repository/note_repository.dart';
+import 'package:flutter_noteapp/presentation/add_edit_note/add_edit_note_view_model.dart';
+import 'package:flutter_noteapp/presentation/notes/notes_view_model.dart';
+import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
+import 'package:sqflite/sqflite.dart';
 
-List<SingleChildWidget> globalProviders = [
-  ...independentModels,
-  ...dependentModels,
-  ...viewModels
-];
+Future<List<SingleChildWidget>> getProviders() async {
+  Database database =
+      await openDatabase('notes_db', version: 1, onCreate: (db, version) async {
+    await db.execute(
+        'CREATE TABLE note (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, content TEXT, color INTEGER, timestamp INTEGER)');
+  });
 
-List<SingleChildWidget> independentModels = [];
+  NoteDbHelper noteDbHelper = NoteDbHelper(database);
+  NoteRepository repository = NoteRepositoryImpl(noteDbHelper);
+  NotesViewModel notesViewModel = NotesViewModel(repository);
+  AddEditNoteViewModel addEditNoteViewModel = AddEditNoteViewModel(repository);
 
-List<SingleChildWidget> dependentModels = [];
-
-List<SingleChildWidget> viewModels = [];
+  return [
+    ChangeNotifierProvider(create: (_) => notesViewModel),
+    ChangeNotifierProvider(create: (_) => addEditNoteViewModel),
+  ];
+}
